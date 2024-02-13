bc33ed09c1457ee98547480297708e13e6415c25
kandi <kandibandasaigopi446@gmail.com>
2024-02-13 09:25:31 +0530

    hi

---
+++ src/StudentTest.java
@@ -0,0 +1,20 @@
+public class StudentTest {
+    public String name;
+    public String fathername;
+boolean login(StudentTest details){
+    String name="sai";
+    String fathername="suresh";
+    if(StudentTest.equals(details.name) && fathername.equals(details.fathername)){
+        return true;
+    }else {
+        return false;
+        public static void main(Strimg args[]){
+            StudentTest studentTest= new StudentTest();
+            system.out.println("stacktest");
+
+        }
+    }
+
+}
+
+}
